618ad6f299b39d3aa40a465cff5ada87cabbed04
설 한 <5fz6hc6psp@privaterelay.appleid.com>
2023-05-04 21:55:47 +0900

    Initial Commit

---
+++ 230504 algorithm closet/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  230504 algorithm closet
+//
+//  Created by 한설 on 2023/05/04.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
